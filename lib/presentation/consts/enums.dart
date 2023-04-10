b245a95730554fb39813d6c15730b0cf901702a4
Neikan <sladkov.e.m@outlook.com>
2023-04-10 14:46:31 +0300

    feat(presentation): add consts

---
+++ lib/presentation/consts/enums.dart
@@ -0,0 +1,1 @@
+enum TabBottomBar { expenses, profile }
---
+++ lib/presentation/consts/translations.dart
@@ -0,0 +1,25 @@
+// Project imports:
+import 'package:fmt/presentation/consts/enums.dart';
+import 'package:fmt/presentation/consts/keys.dart';
+
+final labelsCategories = <String, String>{
+  keyContent: 'За ... нет расходов',
+  TabBottomBar.expenses.name: 'Расходы',
+  TabBottomBar.profile.name: 'Профиль',
+};
+
+final labelsBottomBar = <TabBottomBar, String>{
+  TabBottomBar.expenses: 'Расходы',
+  TabBottomBar.profile: 'Профиль',
+};
+
+const labelsProfile = <String, String>{
+  keyTitle: 'Профиль',
+  keyButtonSave: 'Сохранить',
+  keyButtonSignout: 'Выйти',
+};
+
+const labelsNotFound = <String, String>{
+  keyTitle: 'Ошибка',
+  keyContent: 'Страница на найдена',
+};
