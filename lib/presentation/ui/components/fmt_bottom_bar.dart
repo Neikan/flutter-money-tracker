4cf3deb89a5ad5360b76887391d39847e292de64
Neikan <sladkov.e.m@outlook.com>
2023-04-13 11:02:39 +0300

    feat(presentation): update FMTBottonBar

--- lib/presentation/ui/components/fmt_bottom_bar.dart
+++ lib/presentation/ui/components/fmt_bottom_bar.dart
@@ -4,13 +4,18 @@ import 'package:fmt/presentation/consts/enums.dart';
 import 'package:fmt/presentation/consts/translations.dart';
 
 class FMTBottonBar extends StatelessWidget {
-  const FMTBottonBar({super.key});
+  final TabBottomBar tab;
+
+  const FMTBottonBar({
+    super.key,
+    required this.tab,
+  });
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       onTap: (index) {},
-      currentIndex: 1,
+      currentIndex: TabBottomBar.values.indexOf(tab),
       items: [
         BottomNavigationBarItem(
           icon: const Icon(Icons.credit_card),
