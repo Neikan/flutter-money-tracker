07d48ea5f0e4c2bc9eea5ea6e8702c31d4618d79
Neikan <sladkov.e.m@outlook.com>
2023-04-13 11:03:44 +0300

    feat(presentation): update styles

--- lib/presentation/ui/styles/themes.dart
+++ lib/presentation/ui/styles/themes.dart
@@ -1,24 +1,72 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fmt/presentation/ui/styles/colors.dart' as colors;
 
 final defaultTheme = ThemeData(
+  colorScheme: const ColorScheme.light(
+    primary: colors.base,
+  ),
+  fontFamily: 'SF UI Display',
   appBarTheme: appBarTheme,
   primaryColor: Colors.white,
-  primarySwatch: Colors.deepPurple,
+  // primarySwatch: Colors.deepPurple,
   visualDensity: VisualDensity.adaptivePlatformDensity,
   iconTheme: iconTheme,
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(
+      fontSize: 17.0,
+      height: 1.0,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 17.0,
+      height: 1.0,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 17.0,
+      height: 1.0,
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      textStyle: MaterialStateProperty.all<TextStyle>(
+        const TextStyle(
+          fontSize: 17.0,
+          height: 1.0,
+        ),
+      ),
+      shape: MaterialStateProperty.all<OutlinedBorder?>(
+        const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(15.0),
+          ),
+        ),
+      ),
+    ),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all<EdgeInsetsGeometry?>(EdgeInsets.zero),
+      textStyle: MaterialStateProperty.all<TextStyle>(
+        const TextStyle(
+          fontSize: 17.0,
+          height: 1.0,
+        ),
+      ),
+    ),
+  ),
 );
 
 const appBarTheme = AppBarTheme(
   systemOverlayStyle: SystemUiOverlayStyle(
-    statusBarColor: Colors.deepPurple,
+    statusBarColor: colors.base,
     statusBarIconBrightness: Brightness.light,
     statusBarBrightness: Brightness.light,
   ),
   titleTextStyle: TextStyle(
     color: Colors.white,
-    fontSize: 20,
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
   ),
 );
 
