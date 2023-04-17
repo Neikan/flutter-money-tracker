488dae6ac104cb4266a135b3a6a326a2bb99e290
Neikan <sladkov.e.m@outlook.com>
2023-04-17 13:54:02 +0300

    feat(presentation): add handlers to FMTBottonBar

--- lib/presentation/ui/components/fmt_bottom_bar.dart
+++ lib/presentation/ui/components/fmt_bottom_bar.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // Project imports:
 import 'package:fmt/presentation/consts/enums.dart';
+import 'package:fmt/presentation/consts/routes.dart';
 import 'package:fmt/presentation/consts/translations.dart';
 
 class FMTBottonBar extends StatelessWidget {
@@ -15,9 +16,21 @@ class FMTBottonBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final currentIndex = TabBottomBar.values.indexOf(tab);
+
+    void handleTap(int index) {
+      if (index == 0 && currentIndex != 0) {
+        Navigator.pushReplacementNamed(context, routeSummary);
+      }
+
+      if (index == 1 && currentIndex != 1) {
+        Navigator.pushReplacementNamed(context, routeProfile);
+      }
+    }
+
     return BottomNavigationBar(
-      onTap: (index) {},
-      currentIndex: TabBottomBar.values.indexOf(tab),
+      onTap: handleTap,
+      currentIndex: currentIndex,
       items: [
         BottomNavigationBarItem(
           icon: const Icon(Icons.credit_card),
