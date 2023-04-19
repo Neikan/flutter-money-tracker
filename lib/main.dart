c58ff460e7fe53c97888f7fa5d0297c16b1197ca
Neikan <sladkov.e.m@outlook.com>
2023-04-19 15:20:04 +0300

    feat(main, presentation): add new routes and blocs

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Project imports:
 import 'package:fmt/data/repositories/repository_categories/repository_categories_imp.dart';
-import 'package:fmt/domain/blocs/bloc_category/bloc_categories.dart';
-import 'package:fmt/domain/blocs/bloc_category/bloc_categories_event.dart';
+import 'package:fmt/data/repositories/repository_spendings/repository_spendings_imp.dart';
+import 'package:fmt/domain/blocs/bloc_categories/bloc_categories.dart';
+import 'package:fmt/domain/blocs/bloc_categories/bloc_categories_event.dart';
+import 'package:fmt/domain/blocs/bloc_spendings/bloc_spendings.dart';
 import 'package:fmt/presentation/ui/components/fmt_app.dart';
-import 'package:fmt/presentation/ui/screens/screen_summary/screen_summary.dart';
 
 void main() async {
   runApp(
@@ -18,7 +19,9 @@ void main() async {
         BlocProvider<BlocCategories>(
           create: (_) => BlocCategories(repo: const RepositoryCategoriesImp())
             ..add(BlocCategoriesEventInit()),
-          child: const ScreenSummary(),
+        ),
+        BlocProvider<BlocSpendings>(
+          create: (_) => BlocSpendings(repo: const RepositorySpendingsImp()),
         ),
       ],
       child: const FMTApp(),
--- lib/presentation/utils/routes.dart
+++ lib/presentation/utils/routes.dart
@@ -5,12 +5,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Project imports:
+import 'package:fmt/data/models/app_category/app_category.dart';
 import 'package:fmt/data/repositories/repository_profile/repository_profile_imp.dart';
 import 'package:fmt/domain/blocs/bloc_profile/bloc_profile.dart';
 import 'package:fmt/domain/blocs/bloc_profile/bloc_profile_event.dart';
+import 'package:fmt/domain/blocs/bloc_spendings/bloc_spendings.dart';
+import 'package:fmt/domain/blocs/bloc_spendings/bloc_spendings_event.dart';
+import 'package:fmt/presentation/consts/keys.dart';
 import 'package:fmt/presentation/consts/routes.dart';
 import 'package:fmt/presentation/ui/screens/screen_not_found.dart';
 import 'package:fmt/presentation/ui/screens/screen_profile/screen_profile.dart';
+import 'package:fmt/presentation/ui/screens/screen_spendings/screen_spendings.dart';
 import 'package:fmt/presentation/ui/screens/screen_summary/screen_summary.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) => MaterialPageRoute(
@@ -19,6 +24,14 @@ Route<dynamic> generateRoute(RouteSettings settings) => MaterialPageRoute(
         switch (settings.name) {
           case routeSummary:
             return const ScreenSummary();
+
+          case routeSpendings:
+            final category = (settings.arguments
+                as Map<String, AppCategory>)[keyArgsCategory]!;
+
+            context.read<BlocSpendings>().add(BlocSpendingsEventInit(category));
+
+            return ScreenCategory(category: category);
 
           case routeProfile:
             return BlocProvider<BlocProfile>(
