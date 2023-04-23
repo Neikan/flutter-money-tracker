286b9c5479b62d5e3516f73ee8ae151d7c80545d
Neikan <sladkov.e.m@outlook.com>
2023-04-23 13:35:56 +0300

    feat(main): add BlocDate

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Project imports:
 import 'package:fmt/data/repositories/repository_categories/repository_categories_imp.dart';
+import 'package:fmt/data/repositories/repository_date/repository_date_imp.dart';
 import 'package:fmt/data/repositories/repository_spendings/repository_spendings_imp.dart';
 import 'package:fmt/domain/blocs/bloc_categories/bloc_categories.dart';
 import 'package:fmt/domain/blocs/bloc_categories/bloc_categories_event.dart';
+import 'package:fmt/domain/blocs/bloc_date/bloc_date.dart';
+import 'package:fmt/domain/blocs/bloc_date/bloc_date_event.dart';
 import 'package:fmt/domain/blocs/bloc_spendings/bloc_spendings.dart';
 import 'package:fmt/presentation/ui/components/fmt_app.dart';
 
@@ -16,6 +19,10 @@ void main() async {
   runApp(
     MultiBlocProvider(
       providers: [
+        BlocProvider<BlocDate>(
+          create: (_) => BlocDate(repo: const RepositoryDateImp())
+            ..add(BlocDateEventInit()),
+        ),
         BlocProvider<BlocCategories>(
           create: (_) => BlocCategories(repo: const RepositoryCategoriesImp())
             ..add(BlocCategoriesEventInit()),
